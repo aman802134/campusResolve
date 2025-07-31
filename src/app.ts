27983e48d936ca83f1f77685bd1e489712a51318
aman802134 <pandeyamankumar8@gmail.com>
2025-07-31 22:11:16 +0530

    fix : cors err

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 
 //db connection
 connectDB();
